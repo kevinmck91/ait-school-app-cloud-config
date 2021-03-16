1678ac1bdcfee95064f65298b98df4a6745c12f2
kevmc <kevmckeon@gmail.com>
2021-03-16 22:22:02 +0000

    Initial commit

---
+++ src/test/java/com/school/config/SpringBootconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
