2eee23f775211c371393774825a030a80402d64f
kevmc <kevmckeon@gmail.com>
2021-04-05 19:36:25 +0100

    Configure for Eureka

--- src/main/java/com/school/config/ConfigApplication.java
+++ src/main/java/com/school/config/ConfigApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class SpringBootconfigApplication {
+public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootconfigApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
 
 }
